82d53c4c3435732d73629a1afbc6b34b9543b1c6
Juan Vega <juanjvega13@gmail.com>
2025-03-25 18:04:00 -0500

    calculateCalories function running

--- script.js
+++ script.js
@@ -46,3 +46,21 @@ function getCaloriesFromInput(list) {
   }
   return calories;
 }
+
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+  const breakfastInputs = document.querySelectorAll(
+    `#breakfast input[type='number']`
+  );
+  const lunchInputs = document.querySelectorAll(`#lunch input[type='number']`);
+  const dinnerNumberInputs = document.querySelectorAll(
+    "#dinner input[type='number']"
+  );
+  const snacksNumberInputs = document.querySelectorAll(
+    "#snacks input[type='number']"
+  );
+  const exerciseNumberInputs = document.querySelectorAll(
+    "#exercise input[type='number']"
+  );
+}
