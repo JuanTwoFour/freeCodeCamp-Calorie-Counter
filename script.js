7989a7fb799a6a778d35a3a049806a751c2dd9f5
Juan Vega <juanjvega13@gmail.com>
2025-03-24 19:34:48 -0500

    final details add to addEntry function"

--- script.js
+++ script.js
@@ -22,5 +22,9 @@ function addEntry() {
   );
   const entryNumber =
     targetInputContainer.querySelectorAll('input[type="text"]').length;
-  const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name"> Entry ${entryNumber} Name</label>`;
+  const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name"> Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" name="${entryDropdown.value}-${entryNumber}-name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
+  `;
 }
